486d1124b7d8568df36418c839dbfb7d70956d0c
Ketho <sghs17@outlook.com>
2020-07-31 20:53:54 +0200

    Initial commit

---
+++ HideTutorial.lua
@@ -0,0 +1,19 @@
+-- License: Public Domain
+
+local function OnEvent(self, event)
+	SetCVar("showTutorials", 0)
+	SetCVar("showNPETutorials", 0)
+
+	-- help plates
+	for i = 1, NUM_LE_FRAME_TUTORIALS do
+		C_CVar.SetCVarBitfield("closedInfoFrames", i, true)
+	end
+
+	for i = 1, NUM_LE_FRAME_TUTORIAL_ACCCOUNTS do
+		C_CVar.SetCVarBitfield("closedInfoFramesAccountWide", i, true)
+	end
+end
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("VARIABLES_LOADED")
+f:SetScript("OnEvent", OnEvent)
