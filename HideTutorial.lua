e0dcbe0677be6a138b92f79ab7cc78ea63f29209
Ketho <sghs17@outlook.com>
2020-10-21 02:35:25 +0200

    Only set the cvars once per character

--- HideTutorial.lua
+++ HideTutorial.lua
@@ -1,5 +1,4 @@
 -- License: Public Domain
-
 local pendingChanges
 
 -- VARIABLES_LOADED seems to consistently fire after ADDON_LOADED
@@ -14,12 +13,10 @@ local function OnEvent(self, event, addon)
 			SetCVar("showTutorials", 0)
 			SetCVar("showNPETutorials", 0)
 			SetCVar("hideAdventureJournalAlerts", 1)
-
 			-- help plates
 			for i = 1, NUM_LE_FRAME_TUTORIALS do
 				C_CVar.SetCVarBitfield("closedInfoFrames", i, true)
 			end
-
 			for i = 1, NUM_LE_FRAME_TUTORIAL_ACCCOUNTS do
 				C_CVar.SetCVarBitfield("closedInfoFramesAccountWide", i, true)
 			end
