4558384f1a3a6202afc82549833e894fe027004c
Ketho <sghs17@outlook.com>
2020-10-21 02:39:02 +0200

    Only set the cvars once per character

--- HideTutorial.lua
+++ HideTutorial.lua
@@ -1,21 +1,30 @@
 -- License: Public Domain
+local pendingChanges
 
-local function OnEvent(self, event)
-	SetCVar("showTutorials", 0)
-	SetCVar("showNPETutorials", 0)
-	SetCVar("hideAdventureJournalAlerts", 1)
-
-	-- help plates
-	for i = 1, NUM_LE_FRAME_TUTORIALS do
-		C_CVar.SetCVarBitfield("closedInfoFrames", i, true)
-	end
-
-	for i = 1, NUM_LE_FRAME_TUTORIAL_ACCCOUNTS do
-		C_CVar.SetCVarBitfield("closedInfoFramesAccountWide", i, true)
+-- VARIABLES_LOADED seems to consistently fire after ADDON_LOADED
+local function OnEvent(self, event, addon)
+	if event == "ADDON_LOADED" and addon == "HideTutorial" then
+		if not HideTutorialDB then -- only do this once per character
+			HideTutorialDB = true
+			pendingChanges = true
+		end
+	elseif event == "VARIABLES_LOADED" then
+		if pendingChanges then
+			C_CVar.SetCVar("showTutorials", 0)
+			C_CVar.SetCVar("showNPETutorials", 0)
+			C_CVar.SetCVar("hideAdventureJournalAlerts", 1)
+			-- help plates
+			for i = 1, NUM_LE_FRAME_TUTORIALS do
+				C_CVar.SetCVarBitfield("closedInfoFrames", i, true)
+			end
+			for i = 1, NUM_LE_FRAME_TUTORIAL_ACCCOUNTS do
+				C_CVar.SetCVarBitfield("closedInfoFramesAccountWide", i, true)
+			end
+		end
 	end
 end
 
 local f = CreateFrame("Frame")
+f:RegisterEvent("ADDON_LOADED")
 f:RegisterEvent("VARIABLES_LOADED")
 f:SetScript("OnEvent", OnEvent)
---
