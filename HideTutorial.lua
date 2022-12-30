b75e63b4112d0550ffd87aa71a5bfb1cfed333f4
Ketho <sghs17@outlook.com>
2022-12-30 10:11:38 +0100

    Register hideHelptips cvar

--- HideTutorial.lua
+++ HideTutorial.lua
@@ -10,11 +10,12 @@ local function OnEvent(self, event, addon)
 			pendingChanges = true
 		end
 	elseif event == "VARIABLES_LOADED" then
+		C_CVar.SetCVar("showTutorials", 0)
+		C_CVar.SetCVar("showNPETutorials", 0)
+		C_CVar.SetCVar("hideAdventureJournalAlerts", 1)
+		C_CVar.RegisterCVar("hideHelptips", 1)
 		local lastInfoFrame = C_CVar.GetCVarBitfield("closedInfoFrames", NUM_LE_FRAME_TUTORIALS)
 		if pendingChanges or not lastInfoFrame then
-			C_CVar.SetCVar("showTutorials", 0)
-			C_CVar.SetCVar("showNPETutorials", 0)
-			C_CVar.SetCVar("hideAdventureJournalAlerts", 1)
 			-- help plates
 			for i = 1, NUM_LE_FRAME_TUTORIALS do
 				C_CVar.SetCVarBitfield("closedInfoFrames", i, true)
